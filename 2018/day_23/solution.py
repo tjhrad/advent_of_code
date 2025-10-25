a90cec5d0a1369c37ffc69ba6c47d69b75be3707
tjhrad <14080006+tjhrad@users.noreply.github.com>
2025-10-24 20:51:16 -0700

    Part 2 of day 23. Fixed heapq

--- 2018/day_23/solution.py
+++ 2018/day_23/solution.py
@@ -2,6 +2,7 @@
 
 import helpers
 from collections import defaultdict
+import heapq
 
 
 def part_1(data):
@@ -12,41 +13,93 @@ def part_1(data):
     for r, pos in nanobots:
         if max_range != r:
             continue
-        
+
         for r_2, pos_2 in nanobots:
             dist = sum(abs(a-b) for a, b in zip(pos, pos_2))
             if dist <= max_range:
                 answer += 1
-    
+
     print(f"\nPart 1: {answer}")
 
 
 def part_2(data):
-    # TODO: Brute force too slow
-    # Learn how to implement octree/space subdivition approach
-
     nanobots = get_bots(data)
-
-    counts = defaultdict(int)
     max_x = max(list(pos)[0] for _, pos in nanobots)
+    min_x = min(list(pos)[0] for _, pos in nanobots)
+
     max_y = max(list(pos)[1] for _, pos in nanobots)
+    min_y = min(list(pos)[1] for _, pos in nanobots)
+    
     max_z = max(list(pos)[2] for _, pos in nanobots)
+    min_z = min(list(pos)[2] for _, pos in nanobots)
 
-    for x in range(max_x + 1):
-        for y in range(max_y + 1):
-            for z in range(max_z + 1):
-                coordinate = (x, y, z)
+    # cube = (x, y, z, size)
+    cube_size = 1
+    while cube_size < max(max_x - min_x, max_y - min_y, max_z - min_z):
+        cube_size *= 2
 
-                for r, pos in nanobots:
-                    dist = sum(abs(a-b) for a, b in zip(pos, coordinate))
-                    if dist <= r:
-                        counts[coordinate] += 1
+    cube_center = ((max_x + min_x)//2, (max_y + min_y)//2, (max_z + min_z)//2)
+    starting_cube = (*cube_center, cube_size)
+    queue = []
 
+    # Create a heapq with subdivisions of the larger cube
+    # Sorts the heap by max value (number of bots in range)
+    # Use a negative value as the priority value (because we actually want a max-heap)
+    distance_to_origin = sum(abs(a-b) for a, b in zip((0, 0, 0), cube_center))
+    heapq.heappush(queue, ((-len(nanobots), starting_cube[3], distance_to_origin), starting_cube))
 
-    max_count = max([count for count in counts.values()])
-    coordinates = [p for p, v in counts.items() if v == max_count]
-    answer = min([sum(abs(a-b) for a, b in zip((0, 0, 0), p)) for p in coordinates])
+    answer = 0
+    while queue:
+        # TODO: Fix the heapq pop based on the new tuple
+        _, cube = heapq.heappop(queue)
+
+        if cube[3] <= 1: # Done
+            print((cube[0], cube[1], cube[2]))
+            answer = sum(abs(a-b) for a, b in zip((cube[0], cube[1], cube[2]), (0,0,0)))
+            break
+
+        # Subdivide the cube into 8 equal sub-cubes
+        for subcube in subdivide_cube(*cube):
+            # Find how many robots are in the current subcube
+            
+            n_collisions = 0
+            for bot in nanobots:
+                # Check if bot is in range of the subcube
+                if collision((subcube[0], subcube[1], subcube[2]), subcube[3], list(bot[1]), bot[0]):
+                    n_collisions += 1
+            
+            if n_collisions > 0:
+                distance_to_origin = sum(abs(a-b) for a, b in zip((0, 0, 0), (subcube[0], subcube[1], subcube[2])))
+                heapq.heappush(queue, ((-n_collisions, subcube[3], distance_to_origin), subcube))
+    
     print(f"\nPart 2: {answer}")
+
+
+def collision(cube_center, cube_size, bot_pos, bot_range):
+    half = cube_size / 2
+    xmin, xmax = cube_center[0] - half, cube_center[0] + half
+    ymin, ymax = cube_center[1] - half, cube_center[1] + half
+    zmin, zmax = cube_center[2] - half, cube_center[2] + half
+    
+    dx = 0 if xmin <= bot_pos[0] <= xmax else min(abs(bot_pos[0] - xmin), abs(bot_pos[0] - xmax))
+    dy = 0 if ymin <= bot_pos[1] <= ymax else min(abs(bot_pos[1] - ymin), abs(bot_pos[1] - ymax))
+    dz = 0 if zmin <= bot_pos[2] <= zmax else min(abs(bot_pos[2] - zmin), abs(bot_pos[2] - zmax))
+
+    return (dx + dy + dz) <= bot_range
+
+
+
+def subdivide_cube(x, y, z, size):
+    half = size // 2
+
+    if half == 0:
+        return # Cannot subdivide anymore
+
+    quarter = half // 2
+    for dx in (-quarter, quarter):
+        for dy in (-quarter, quarter):
+            for dz in (-quarter, quarter):
+                yield (x + dx, y + dy, z + dz, half)
 
 
 def get_bots(data):
@@ -60,7 +113,7 @@ def get_bots(data):
     return nanobots
 
 
-with open("./input.txt", "r") as f:
+with open("./test.txt", "r") as f:
     data = [line.rstrip('\n') for line in f]
 
 helpers.time_it(part_1, data)
