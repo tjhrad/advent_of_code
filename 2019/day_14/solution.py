216a2e74e75c15efccf018b9f1518a0ed3320c4c
tjhrad <14080006+tjhrad@users.noreply.github.com>
2025-11-16 17:48:00 -0700

    day 14

--- 2019/day_14/solution.py
+++ 2019/day_14/solution.py
@@ -2,6 +2,7 @@
 
 import helpers
 from collections import defaultdict
+import math
 
 
 def part1(data):
@@ -11,7 +12,22 @@ def part1(data):
 
 
 def part2(data):
-    answer = 0
+    reactions = parse_reactions(data)
+    target = 1000000000000
+    high = 10_000_000
+    low = 0
+
+    while low <= high:
+        mid = (low + high) // 2
+        ore = ore_required('FUEL', mid, reactions, defaultdict(int))
+
+        if ore <= target:
+            best = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    answer = best
     print(f"\nPart 2: {answer}")
 
 
@@ -19,11 +35,24 @@ def ore_required(target, amount, reactions, leftovers):
     if target == 'ORE':
         return amount
 
+    output_quantity, inputs = reactions[target]
 
-    for name, quantity in reactions:
-        print(":)")
+    if leftovers[target] >= amount:
+        leftovers[target] -= amount
+        return 0
+
+    amount -= leftovers[target]
+    leftovers[target] = 0
+
+    batches = math.ceil(amount/output_quantity)
+    ore = 0
+
+    for name, quantity in inputs:
+        ore += ore_required(name, quantity * batches, reactions, leftovers)
         
-    return 0
+    leftovers[target] += batches * output_quantity - amount
+
+    return ore
 
 def parse_reactions(data):
     reactions = {}
@@ -34,7 +63,7 @@ def parse_reactions(data):
         reactions[output_name] = (int(output_qty), inputs)
     return reactions
 
-with open("./test.txt", "r") as f:
+with open("./input.txt", "r") as f:
     data = [line.rstrip('\n') for line in f]
 
 
